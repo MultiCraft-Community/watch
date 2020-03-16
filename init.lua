d770651bb52cd73171051762874c73c42ecb82e4
MoNTE48 <MoNTE48@mail.ua>
2020-03-16 11:44:12 +0100

    Initial release

---
+++ init.lua
@@ -0,0 +1,37 @@
+for hour = 0, 12 do
+	local img = hour ~= 0 and "watch_" .. hour or "blank"
+	minetest.register_tool("watch:" .. hour, {
+		description = "Watch",
+		inventory_image = "watch_watch.png^" .. img .. ".png",
+		groups = {watch = hour, not_in_creative_inventory = (hour == 0 and 0) or 1}
+	})
+end
+
+minetest.register_craft({
+	output = "watch:0",
+	recipe = {
+		{"", "default:gold_ingot", ""},
+		{"default:gold_ingot", "efault:mese_crystal_fragment", "default:gold_ingot"},
+		{"", "default:gold_ingot", ""}
+	}
+})
+
+local floor = math.floor
+local get_timeofday = minetest.get_timeofday
+local registered_tools = minetest.registered_tools
+local get_player_by_name = minetest.get_player_by_name
+
+minetest.register_globalstep(function (dtime)
+	local now = floor((get_timeofday() * 24) % 12)
+	local players  = minetest.get_connected_players()
+	for _, player in pairs(players) do
+		if not player or not player:is_player() then return end
+		local inv = player:get_inventory()
+		for i, stack in pairs(inv:get_list("main")) do
+			local tools = registered_tools[stack:get_name()]
+			if tools and tools.groups.watch and tools.groups.watch ~= now then
+				inv:set_stack("main", i, "watch:" .. now)
+			end
+		end
+	end
+end)
